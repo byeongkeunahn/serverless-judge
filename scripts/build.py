06d0bc24eeca077880d093517562cfe4fe256d7c
Byeongkeun Ahn <7p54ks3@naver.com>
2023-12-07 23:58:38 +0900

    Generate index.html

--- scripts/build.py
+++ scripts/build.py
@@ -38,24 +38,33 @@ def build(src, dst):
                            raise Exception("build: {0} contains invalid test type \"{1}\"".format(manifest_path, test["type"]))
             case _:
                 raise Exception("build: {0} contains invalid key \"{1}\"".format(manifest_path, k))
-    with open("/".join([dst, "manifest.json"]), 'w') as f:
+    compiled_manifest_path = "/".join([dst, "manifest.json"])
+    with open(compiled_manifest_path, 'w') as f:
         f.write(json.dumps(output, indent=4))
+    return '    <a href="judge.html?manifest={0}">Test for {1}</a><br>\n'.format(compiled_manifest_path, output["metadata"]["src"])
 
 def discover(src, dst):
+    out = []
     os.makedirs(dst, exist_ok=True)
     manifest_path = os.path.join(src, "manifest.json")
     if os.path.isfile(manifest_path):
-        build(src, dst)
+        out.append(build(src, dst))
     else:
         for f in os.listdir(src):
             g = "/".join([src, f])
             if os.path.isdir(g):
-                discover(g, "/".join([dst, f]))
+                out += discover(g, "/".join([dst, f]))
+    return out
 
 def run(src, dst):
     shutil.rmtree(dst)
     os.mkdir(dst)
-    discover(src, dst)
+    out = discover(src, dst)
+    with open("scripts/index-template.html", 'r') as f:
+        index_template = f.read()
+    index_template = index_template.replace("$$$$judge_list$$$$", "".join(out))
+    with open("index.html", "w") as f:
+        f.write(index_template)
 
 if __name__ == "__main__":
     run("database", "database-compiled")
