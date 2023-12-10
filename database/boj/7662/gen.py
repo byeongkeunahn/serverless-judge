f5c3c8a8db0b71c91937978c7b3e99c26d70558f
Byeongkeun Ahn <7p54ks3@naver.com>
2023-12-10 15:44:00 +0900

    Strengthen stress test for 7662

--- database/boj/7662/gen.py
+++ database/boj/7662/gen.py
@@ -14,14 +14,24 @@ def generate():
   else:
     MIN, MAX = -(2**31), (2**31) - 1
   queries = random.randint(1, 100)
-  input_str = ["{0}\n".format(queries)]
+  input_str = []
+  th = random.randint(3, 7)
+  cnt = 0
   for _ in range(queries):
-    t = random.randint(0, 1)
-    if t == 0:
+    t = random.randint(0, 9)
+    if t < th:
       input_str.append("I {0}\n".format(random.randint(MIN, MAX)))
+      cnt += 1
     else:
       input_str.append("D {0}\n".format(random.randint(0, 1) * 2 - 1))
-  return "".join(input_str)
+      cnt = max(cnt - 1, 0)
+  if random.randint(0, 1) == 1:
+    c = random.randint(2, 10)
+    while cnt > c:
+      input_str.append("D {0}\n".format(random.randint(0, 1) * 2 - 1))
+      cnt = max(cnt - 1, 0)
+      queries += 1
+  return "".join(["{0}\n".format(queries)] + input_str)
 
 out = ["{0}\n".format(test_cases)]
 for _ in range(test_cases):
